17c55fc1f6e1dfe461b96a119627e27ee582ca5d
polgallardo <pol.gallardo96@gmail.com>
2023-06-09 15:41:43 +0200

    agregar pages

---
+++ src/pages/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { ThemeProvider } from 'theme-ui';
+import theme from 'theme';
+import SEO from 'components/seo';
+import Layout from 'components/layout';
+import Banner from 'sections/banner';
+import TeamSection from '../sections/team-section'; 
+import OurCustomer from 'sections/our-customer';
+import Users from 'sections/users';
+import Create from 'sections/create';
+import Faq from 'sections/faq';
+
+export default function IndexPage() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <SEO title="BitHire | El mejor sitio para los talentos y las empresas" />
+        <Banner />
+        <TeamSection />
+        <OurCustomer />
+        <Users />
+        <Create />
+        <Faq />
+      </Layout>
+    </ThemeProvider>
+  );
+}
